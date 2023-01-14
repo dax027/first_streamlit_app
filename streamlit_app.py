bb349a0ebe6bdc2da35601e82cc8f4461885f8fe
Dax Dickenson <dax027@gmail.com>
2023-01-14 16:12:31 -0500

    Update streamlit_app.py
    normalized json
    new dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,3 +26,8 @@ streamlit.header('Fruityvice Fruit Advice!')
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# normalize json response
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# put normalized json in dataframe
+streamlit.dataframe(fruityvice_normalized)
